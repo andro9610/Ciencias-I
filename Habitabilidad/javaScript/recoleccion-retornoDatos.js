2728e757e5928f7af2f1b66c4521b28808919f30
andro9610 <andro9610@gmail.com>
2018-06-06 00:52:54 -0500

    añadido recoleccion de datos
    Se recolectan los datos de las paginas index.html y gestionHabitantes.html

--- Habitabilidad/javaScript/recoleccion-retornoDatos.js
+++ Habitabilidad/javaScript/recoleccion-retornoDatos.js
@@ -1,5 +1,6 @@
 /* Recoleccion y retorno de datos (devuelve los datos para mostrar la habitabilidad segun un API) */
 
+/*para index.html */
 function recolectarFenomenos(){
   var fenomenoAgregado = document.getElementById("fenomenoAgregado").value;
   var fenomenoQuitado = document.getElementById("fenomenoQuitado").value;
@@ -11,4 +12,29 @@ function recolectarDatosMantenimiento(){
 
   /*Estados de habitacion: 0=Disponible 1=Mantenimiento */
   var estadoHabitacion = document.getElementById("estadoHabitacion").value;
+}
+
+/*Para gestionHabitantes.html */
+function recolectar_añadirHabitantes(){
+  var habitantesAñadidos = document.getElementById("habitantesAñadidos").value;
+  document.getElementById("habitantesAñadidos").innerHTML = habitantesAñadidos;
+
+  var lugarHabitantesAñadidos = document.getElementById("lugarHabitantesAñadidos").value;
+  document.getElementById("lugarHabitantesAñadidos").innerHTML = lugarHabitantesAñadidos;
+}
+
+function recolectar_eliminarHabitantes(){
+  var habitantesEliminados = document.getElementById("habitantesEliminados").value;
+  document.getElementById("habitantesEliminados").innerHTML = habitantesEliminados;
+
+  var lugarHabitantesEliminados = document.getElementById("lugarHabitantesEliminados").value;
+  document.getElementById("lugarHabitantesEliminados").innerHTML = lugarHabitantesEliminados;
+}
+
+function recolectar_mudarHabitantes(){
+  var lugarOrigen = document.getElementById("lugarOrigen").value;
+  document.getElementById("lugarOrigen").innerHTML = lugarOrigen;
+
+  var lugarDestino = document.getElementById("lugarDestino").value;
+  document.getElementById("lugarDestino").innerHTML = lugarDestino;
 }
