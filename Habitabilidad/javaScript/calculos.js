b7d7f79802d4a9cb2cfeaf6ff2e12c261f683ee3
andro9610 <andro9610@gmail.com>
2018-06-06 14:30:34 -0500

    añadido calculos.Js
    se añade la funcion principal del proyecto

---
+++ Habitabilidad/javaScript/calculos.js
@@ -0,0 +1,6 @@
+/* Funcion principal de todo el proyecto:
+   esta funcion define de que manera se actualiza la habitabilidad */
+function actualizarHabitabilidad(){
+    tituloPagina = document.getElementsByTagName("tittle");
+    alert(tituloPagina);
+}
